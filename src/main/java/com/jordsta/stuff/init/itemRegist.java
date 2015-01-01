b995553c4de2e22187b3dd48b9f0b72320a5ef9a
Elec332 <arnout1998@gmail.com>
2015-01-01 16:43:10 +0100

    Fix crashes
    *cough* @jordsta95 *cough*

--- src/main/java/com/jordsta/stuff/init/itemRegist.java
+++ src/main/java/com/jordsta/stuff/init/itemRegist.java
@@ -5,12 +5,14 @@ import com.jordsta.stuff.items.*;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor.ArmorMaterial;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.util.EnumHelper;
 
 /**
  * Created by Elec332 on 13-12-2014.
  */
 public class itemRegist {
+
     public static final Item.ToolMaterial MONEY = EnumHelper.addToolMaterial("MONEY", 7, -1, 600.0F, 2.0F, 25);
     public static final Item.ToolMaterial MONEY2 = EnumHelper.addToolMaterial("MONEY2", 7, -1, 200.0F, 246.0F, 25);
     public static final Item.ToolMaterial SUPER = EnumHelper.addToolMaterial("SUPER", 7, 2000, 35.0F, 2.0F, 25);
@@ -40,22 +42,22 @@ public class itemRegist {
     
     public static void Register() {
         RegisterHelper.registerItem(unrefinedCurrency = new unrefinedCurrency("unrefinedCurrency"));
-        RegisterHelper.registerItem(moneyPick = new moneyPick(20, MONEY));
+        moneyPick = new moneyPick(20, MONEY);
         RegisterHelper.registerItem(lightDust = new unrefinedCurrency("lightDust"));
        // RegisterHelper.registerItem(ironNugget = new baseItem("ironNugget"));
-        RegisterHelper.registerItem(moneyBlade = new moneyBlade(20, MONEY2));
-        RegisterHelper.registerItem(moneyShovel = new moneyShovel(20, MONEY));
-        RegisterHelper.registerItem(moneyAxe = new moneyAxe(20, MONEY));
+        moneyBlade = new moneyBlade(20, MONEY2);
+        moneyShovel = new moneyShovel(20, MONEY);
+        moneyAxe = new moneyAxe(20, MONEY);
         RegisterHelper.registerItem(diamondRod = new unrefinedCurrency("diamondRod"));
         RegisterHelper.registerItem(superium = new superium("superium"));
-        RegisterHelper.registerItem(superAxe = new superAxe(20, SUPER));
-        RegisterHelper.registerItem(superPick = new superPick(20, SUPER));
-        RegisterHelper.registerItem(superPick = new superShovel(20, SUPER));
-        RegisterHelper.registerItem(superPick = new superSword(20, SUPER2));
+        superAxe = new superAxe(20, SUPER);
+        superPick = new superPick(20, SUPER);
+        superShovel = new superShovel(20, SUPER);
+        superSword = new superSword(20, SUPER2);
         RegisterHelper.registerItem(superChest = new superChest(SuperArmour, 1));
         RegisterHelper.registerItem(superHelmet = new superHelmet(SuperArmour, 0));
         RegisterHelper.registerItem(superLegs = new superLegs(SuperArmour, 2));
         RegisterHelper.registerItem(superBoots = new superBoots(SuperArmour, 3));
-       }
+    }
     
 }
