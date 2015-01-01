957e3395d6fd51d79d45b5f8c35625455ff9b1fd
Elec332 <arnout1998@gmail.com>
2015-01-01 19:46:28 +0100

    Complete cleanup

--- src/main/java/com/jordsta/stuff/Reference.java
+++
@@ -1,9 +0,0 @@
-package com.jordsta.stuff;
-
-public class Reference {
-public static final String MODID = "jordsutils";
-public static final String VERSION = "0.1";
-public static final String CLIENT_PROXY = "com.jordsta.stuff.proxies.ClientProxy";
-public static final String COMMON_PROXY = "com.jordsta.stuff.proxies.CommonProxy";
-
-}
--- src/main/java/com/jordsta/stuff/proxies/ClientProxy.java
+++
@@ -1,7 +0,0 @@
-package com.jordsta.stuff.proxies;
-
-import com.jordsta.stuff.proxies.CommonProxy;
-
-public class ClientProxy extends CommonProxy {
-
-}
